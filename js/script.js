cbf7dddc646526f9e840e9b6fb469811f66908cc
marcossdev <marcospaulosmoreiramp11@gmail.com>
2022-12-13 09:00:30 -0300

    Adiçoes de verificao do cep

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const form = document.getElementById("form-cep");
 
 const stateSpan = document.getElementById("state");
 const citySpan = document.getElementById("city");
+const errP = document.getElementById("err");
 
 //Verificação do input
 function validationCep() {
@@ -12,31 +13,41 @@ function validationCep() {
         cepInput.style.border = "";
         requiredSpan.style.display = "none";
         cepInput.style.animation = "none";
-        fetchCEP(cepInput.value.replace("-", ""));
+        return true;
     } else {
-        stateSpan.innerHTML = "?"
-        citySpan.innerHTML = "?"
+        stateSpan.innerHTML = "?";
+        citySpan.innerHTML = "?";
         cepInput.style.border = "2px solid #dd3b3b";
         requiredSpan.style.display = "block";
         setTimeout(() => (cepInput.style.animation = "tremer 0.1s"), 1);
         cepInput.style.animation = "none";
+        return false;
     }
 }
 
 //Fução de pesquisa
 const fetchCEP = async (cep) => {
     const APIResp = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`);
+    const data = await APIResp.json();
     if (APIResp.status === 200) {
-        const data = await APIResp.json();
         if (data) {
-            stateSpan.innerHTML = data.state
-            citySpan.innerHTML = data.city
-        };
+            stateSpan.innerHTML = data.state;
+            citySpan.innerHTML = data.city;
+            errP.style.display = "none";
+        }
+    } else {
+        if (data) {
+            errP.style.display = "block";
+            setTimeout(() => (errP.style.animation = "tremer 0.1s"), 1);
+            errP.style.animation = "none";
+        }
     }
 };
 
 //Submit event
 form.addEventListener("submit", (evt) => {
     evt.preventDefault();
-    validationCep();
+    if (validationCep() === true) {
+        fetchCEP(cepInput.value.replace("-", ""));
+    }
 });
