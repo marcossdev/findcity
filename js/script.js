aafce1aeabc6f01029d2f4383b749d73be44cfd7
marcossdev <marcospaulosmoreiramp11@gmail.com>
2022-12-13 09:19:02 -0300

    Finalizando fetchCEP()

--- js/script.js
+++ js/script.js
@@ -30,19 +30,18 @@ const fetchCEP = async (cep) => {
     const APIResp = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`);
     const data = await APIResp.json();
     if (APIResp.status === 200) {
-        if (data) {
-            stateSpan.innerHTML = data.state;
-            citySpan.innerHTML = data.city;
-            errP.style.display = "none";
-        }
+        stateSpan.innerHTML = data.state;
+        citySpan.innerHTML = data.city;
+        errP.style.display = "none";
     } else {
-        if (data) {
-            errP.style.display = "block";
-            setTimeout(() => (errP.style.animation = "tremer 0.1s"), 1);
-            errP.style.animation = "none";
-        }
+        stateSpan.innerHTML = "?";
+        citySpan.innerHTML = "?"
+        errP.style.display = "block";
+        setTimeout(() => (errP.style.animation = "tremer 0.1s"), 1);
+        errP.style.animation = "none";
     }
 };
+
 
 //Submit event
 form.addEventListener("submit", (evt) => {
