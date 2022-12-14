2b9a3cee280d9149c9d2066c5f376393ffcc0bf0
marcossdev <marcospaulosmoreiramp11@gmail.com>
2022-12-14 12:32:31 -0300

    Mininal update

--- js/script.js
+++ js/script.js
@@ -7,56 +7,56 @@ const errP = document.getElementById("err");
 
 //Spam defalt :)
 function spanDefault() {
-    return (stateSpan.innerHTML = "?"), (citySpan.innerHTML = "?");
+	return (stateSpan.innerHTML = "?"), (citySpan.innerHTML = "?");
 }
 
 //Verificação do input
 function validationCep() {
-    const regex = /\d{5}-\d{3}/g;
-    if (regex.test(cepInput.value)) {
-        cepInput.style.border = "";
-        requiredSpan.style.display = "none";
-        cepInput.style.animation = "none";
-        return true;
-    } else {
-        spanDefault();
-        cepInput.style.border = "2px solid #dd3b3b";
-        requiredSpan.style.display = "block";
-        setTimeout(() => (cepInput.style.animation = "tremer 0.1s"), 1);
-        cepInput.style.animation = "none";
-        errP.style.display = "none"
-        return false;
-    }
+	const regex = /\d{5}-\d{3}/g;
+	if (regex.test(cepInput.value)) {
+		cepInput.style.border = "";
+		requiredSpan.style.display = "none";
+		cepInput.style.animation = "none";
+		return true;
+	} else {
+		spanDefault();
+		cepInput.style.border = "2px solid #dd3b3b";
+		requiredSpan.style.display = "block";
+		setTimeout(() => (cepInput.style.animation = "tremer 0.1s"), 1);
+		cepInput.style.animation = "none";
+		errP.style.display = "none";
+		return false;
+	}
 }
 
 //CEP Mask
 cepInput.addEventListener("keypress", () => {
-    const ipt = cepInput.value;
-    if (ipt.length === 5) {
-        cepInput.value += "-";
-    }
+	const ipt = cepInput.value;
+	if (ipt.length === 5) {
+		cepInput.value += "-";
+	}
 });
 
 //Fução de pesquisa
 const fetchCEP = async (cep) => {
-    const APIResp = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`);
-    const data = await APIResp.json();
-    if (APIResp.status === 200) {
-        stateSpan.innerHTML = data.state;
-        citySpan.innerHTML = data.city;
-        errP.style.display = "none";
-    } else {
-        spanDefault();
-        errP.style.display = "block";
-        setTimeout(() => (errP.style.animation = "tremer 0.1s"), 1);
-        errP.style.animation = "none";
-    }
+	const APIResp = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`);
+	const data = await APIResp.json();
+	if (APIResp.status === 200) {
+		stateSpan.innerHTML = data.state;
+		citySpan.innerHTML = data.city;
+		errP.style.display = "none";
+	} else {
+		spanDefault();
+		errP.style.display = "block";
+		setTimeout(() => (errP.style.animation = "tremer 0.1s"), 1);
+		errP.style.animation = "none";
+	}
 };
 
 //Submit event
 form.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    if (validationCep() === true) {
-        fetchCEP(cepInput.value.replace("-", ""));
-    }
+	evt.preventDefault();
+	if (validationCep() === true) {
+		fetchCEP(cepInput.value.replace("-", ""));
+	}
 });
